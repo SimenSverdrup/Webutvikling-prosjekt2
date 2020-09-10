5b2663694acf54df4e7f2aaef5b54447f5945fa3
Ingrid Gomo <54813201+IngridGomo@users.noreply.github.com>
2020-09-10 13:57:46 +0200

    lagd grid

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Poetry/>
+        
       </header>
+
+      <div id="header">
+        <p>Tittel</p>
+      </div>
+
+      <div id="grid_container">
+
+        <div id="grid_svg">
+        <svg width="300" height="200" background-color="white">
+          <rect id="house" width="100" height="60" x="100" y="120" />
+          <rect width="5" height="200" x="0" y="0" />
+          <rect width="5" height="200" x="295" y="0" />
+          <rect width="300" height="5" x="0" y="195" />
+          <rect width="300" height="5" x="0" y="0" />
+        </svg>
+        </div>
+
+        <div id="grid_poem">
+          <p>Her kommer det poems</p>
+        </div>
+
+        <div id="grid_sound">
+          <p>Her kommer det lydbar</p>
+        </div>
+
+        <div id="grid_faviourite">
+          <button>Lagre som favoritt</button>
+        </div>
+
+      </div>
+
+      
+
     </div>
   );
 }
