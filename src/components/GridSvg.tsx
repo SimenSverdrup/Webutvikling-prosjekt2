b3aeb83e73f779607136dbf871329618c94ad8de
Theodor Holmsen <theodolh@stud.ntnu.no>
2020-09-21 11:19:40 +0200

    basic logic added issue #11

--- src/components/GridSvg.tsx
+++ src/components/GridSvg.tsx
@@ -1,6 +1,41 @@
 import React from 'react';
+import { GalleryContext } from '../context/GalleryContext';
+import Rocket from "/public/Contents/SVG";
+import Thinking from "/public/Contents/SVG";
+import AnimatedMandala.svg from 'public/Contents/SVG';
 
-export default function GridSvg() {
+
+
+
+const Svg = () => {
+    const {ImgProvider} = React.useContext(GalleryContext);
+    const [data, setData ] = React.useState(Rocket.svg);
+    React.useEffect( () => {
+        if (ImgProvider) {
+            switch (ImgProvider.imgUrl) {
+                case 'Rocket.svg':
+                    setData(Rocket.svg);
+                    break;
+                case 'AnimatedMandala.svg':
+                    setData(AnimatedMandala.svg);
+                    break;
+                case 'Thinking.svg':
+                    setData(Thinking.svg);
+                    break;
+            }
+        }
+    }, [ImgProvider])
+
+    return(<div id="grid_svg">
+        <img src={process.env.PUBLIC_URL + 'Contents/SVG/9va-rocket-blue-and-red.svg'} />
+    </div>);
+}
+
+export default Svg;
+
+
+
+/*export default function GridSvg() {
     return (
         <div id="grid_svg">
 
@@ -8,3 +43,5 @@ export default function GridSvg() {
         </div>
     )
 }
+
+*/
