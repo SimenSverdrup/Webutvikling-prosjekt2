370686eb29c06471d16fbf7f9ad8a5db86affe85
Theodor Holmsen <theodolh@stud.ntnu.no>
2020-09-28 15:26:40 +0200

    final changes added #9

--- src/components/tests/Sidebar.test.tsx
+++ src/components/tests/Sidebar.test.tsx
@@ -13,25 +13,3 @@ it('renders sidebar correctly ', () => {
 
 });
 
-
-/*
-it('renders correctly when there are no assets', () => {
-    const tree = renderer.create(<Sidebar />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
-
-
-it('renders correctly when there is one single asset', () => {
-    const assets = ['one'];
-    const tree = renderer.create(<Listelements assets={assets} />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
-
-
-
-it('renders correctly when there are multiple assets', () => {
-    const assets = ['one', 'two', 'three'];
-    const tree = renderer.create(<Listelements assets={assets} />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
-*/
